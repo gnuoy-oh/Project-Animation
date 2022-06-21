09410091cc1e31d9f053ff223bc468be49b7d4a1
gnuoy-oh <oh.my00418@gmail.com>
2022-06-21 16:31:54 +0900

    update animation-project -1

--- clean-dist.js
+++ clean-dist.js
@@ -5,19 +5,18 @@ const rm = require("rimraf"),
 const destPath = path.resolve(__dirname, "./dist");
 
 const cleanDistFolder = () => {
-  rm(path.join(destPath, "*"), (err) => {
+  rm(path.join(destPath, "*"), err => {
     if (err) {
       console.log(chalk.red(err));
       throw err;
     }
 
     console.log(chalk.white("  Clean Dist Folder\n"));
-
   });
-}
+};
 
 const init = () => {
   cleanDistFolder();
-}
+};
 
 init();
--- webpack.common.js
+++ webpack.common.js
@@ -5,41 +5,30 @@ const path = require("path"),
 
 module.exports = {
   entry: {
-    "main": ["./src/js/index.js"],
+    main: ["./src/js/index.js"]
   },
 
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(js|jsx)$/,
         exclude: /[\\/]node_modules[\\/]/,
         use: {
           loader: "babel-loader",
           options: {
-            "presets": [
+            presets: [
               [
                 "@babel/preset-env",
                 {
-                  "targets": {
-                    "chrome": "58",
-                    "ie": "11"
+                  targets: {
+                    chrome: "58",
+                    ie: "11"
                   }
                 }
               ]
             ]
           }
-        },
-      },
-
-      // Font
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        include: path.resolve(__dirname, "./src/style-core/fonts"),
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "fonts/[name].[ext]",
-          },
-        }, ],
+        }
       },
 
       // SCSS
@@ -50,23 +39,8 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           "css-loader", // css를 읽어들이기
           "postcss-loader", // 벤더프리픽스 대응
-          "sass-loader", // scss to css
-        ],
-      },
-
-      // Image(png) - Core
-      {
-        test: /\.(png|jp(e*)g|gif|ico)$/,
-        include: path.resolve(__dirname, "./src/style-core/images"),
-        use: {
-          loader: "file-loader",
-          options: {
-            outputPath: "./",
-            name: "[path][name].[ext]",
-            // 가져올 경로에서 해당 경로만 지우고 가져온다.
-            context: "src/style-core",
-          },
-        },
+          "sass-loader" // scss to css
+        ]
       },
 
       // Image(png) - Product
@@ -79,26 +53,9 @@ module.exports = {
             outputPath: "./",
             name: "[path][name].[ext]",
             // 가져올 경로에서 해당 경로만 지우고 가져온다.
-            context: "src/",
-          },
-        },
-      },
-
-      // Image(svg) - Core
-      {
-        test: /\.svg$/,
-        include: path.resolve(__dirname, "./src/style-core/images/icon"),
-        use: {
-          loader: "svg-sprite-loader",
-          options: {
-            outputPath: "./",
-            runtimeCompat: true,
-            context: "src/style-core", // 가져올 경로에서 해당 경로만 지우고 가져온다.
-            // extract: true,
-            // spriteFilename: 'images/icon/svg-sprite.svg',
-            // name: "[path][name].[ext]",
-          },
-        },
+            context: "src/"
+          }
+        }
       },
 
       // Image(svg) - Product
@@ -109,40 +66,41 @@ module.exports = {
           loader: "svg-sprite-loader",
           options: {
             outputPath: "./",
-            runtimeCompat: true,
-            context: "src/style-core", // 가져올 경로에서 해당 경로만 지우고 가져온다.
+            runtimeCompat: true
             // extract: true,
             // spriteFilename: 'images/icon/svg-sprite.svg',
             // name: "[path][name].[ext]",
-          },
-        },
-      },
-    ],
+          }
+        }
+      }
+    ]
   },
 
   plugins: [
     // index.js에 포함된 css를 별도의 .css 파일 형식으로 추출한다.
     new MiniCssExtractPlugin({
-      filename: "css/style.min.css",
+      filename: "css/style.min.css"
     }),
 
     // 공통 컴포넌트 영역 모듈화
     new FileIncludeWebpackPlugin({
       source: "./src/html/pages",
-      replace: [{
-        regex: /\[\[FILE_VERSION]]/g,
-        to: "v=1.0.0",
-      }, ],
+      replace: [
+        {
+          regex: /\[\[FILE_VERSION]]/g,
+          to: "v=1.0.0"
+        }
+      ],
       loader: "prettier-loader",
-      destination: "../views",
+      destination: "../views"
     }),
 
-    new SvgSpriteLoaderPlugin({}),
+    new SvgSpriteLoaderPlugin({})
   ],
 
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "./js/[name].bundle.js",
-    publicPath: "../",
-  },
+    publicPath: "../"
+  }
 };
