3700a4d578f912240abb829321112718b5f7cc16
gnuoy-oh <oh.my00418@gmail.com>
2022-07-04 12:14:29 +0900

    feat: 0704 슬라이더 업로드

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const openModalButton = document.querySelector(".component-content__description-button");
+const modalElement = document.querySelector(".modal");
+const closeModalButton = document.querySelector(".modal__head-button");
+
+// 열리기 class 추가
+const modalClose = () => {
+  closeModalButton.addEventListener("click", event => {
+    event.preventDefault();
+
+    modalElement.classList.remove("modal--open");
+  });
+};
+
+// 열리기 class 추가
+const modalOpen = () => {
+  openModalButton.addEventListener("click", event => {
+    event.preventDefault();
+
+    modalElement.classList.add("modal--open");
+  });
+};
+
+const init = () => {
+  modalOpen();
+  modalClose();
+};
+
+init();
